ebcf415b433a4cef492386331500329e7a6f3b1c
Sierra <sonicaradia@gmail.com>
2025-01-17 16:05:10 -0500

    Experimented with loops

--- js/index.js
+++ js/index.js
@@ -1,3 +1,60 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+for (let i = 0; i < 50; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 50; i++) {
+    console.log(i);
+}
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i * 10);
+}
+
+let i = 0;
+
+while (i <= 10) {
+    i++;
+    console.log(i * 10);
+}
+
+
+let i = 0;
+do {
+    i++
+    console.log(i * 10);
+} while (i <= 10);
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(i * 10);
+    if (i === 7) {
+        break;
+    }
+}
+
+
+let i = 0;
+do {
+    i++;
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log(i);
+} while (i <= 10);
+
+
+
+let i = 0;
+do {
+    i++;
+    if (i % 2 != 0) {
+        continue;
+    }
+    console.log(i);
+} while (i <= 10);
+
+*/
